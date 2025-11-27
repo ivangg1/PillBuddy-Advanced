2b3786c554d0e1ee6d752e8ef1bea6fde6b8275e
ivanG <ivangelhorn@gmail.com>
2025-11-27 12:29:04 +0100

    First commit

---
+++ src/test/java/com/grupoX/PillBuddy/PillBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupoX.PillBuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PillBuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
